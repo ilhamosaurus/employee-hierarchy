858db8b0c5310dee645672b66cbb85b0157e970a
Ilhamosaurus <ilhamsssa@gmail.com>
2024-02-21 17:07:18 +0700

    feat: base code initiate

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
-import { EmployeeService } from './employee.service';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { Employee, EmployeeService } from './employee.service';
 
 @Controller('employee')
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
+
+  @Get(':id')
+  getEmployee(@Param('id') id: string): Employee {
+    const employeeId = parseInt(id, 10);
+    const employee = this.employeeService.getEmployeeById(employeeId);
+    if (!employee) {
+      throw new NotFoundException('Employee not found');
+    }
+
+    return employee;
+  }
+
+  @Get(':id/managers')
+  getManagersHierarchy(@Param('id') id: string): Employee[] {
+    const employeeId = parseInt(id, 10);
+    return this.employeeService.getManagersHierarchy(employeeId);
+  }
+
+  @Get(':id/direct-reports-count')
+  getDirectReportsCount(@Param('id') id: string): number {
+    const employeeId = parseInt(id, 10);
+    return this.employeeService.getDirectReportsCount(employeeId);
+  }
+
+  @Get(':id/indirect-reports-count')
+  getIndirectReportsCount(@Param('id') id: string): number {
+    const employeeId = parseInt(id, 10);
+    return this.employeeService.getIndirectReportsCount(employeeId);
+  }
+
+  @Post('initialize')
+  initializeEmployees(@Body() data: Employee[]) {
+    this.employeeService.initializeEmployees(data);
+    return { message: 'Employee data has initializes successfully' };
+  }
 }
--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+  private employees: Map<number, Employee> = new Map();
+  constructor() {}
+
+  initializeEmployees(data: Employee[]) {
+    this.employees.clear();
+    data.forEach((employee) => {
+      this.employees.set(employee.id, { ...employee, directReports: [] });
+      if (employee.managerId !== null) {
+        const manager = this.employees.get(employee.managerId);
+        if (manager) {
+          manager.directReports.push(employee.id);
+        }
+      }
+    });
+  }
+
+  getEmployeeById(id: number): Employee | undefined {
+    return this.employees.get(id);
+  }
+
+  getManagersHierarchy(id: number): Employee[] {
+    const employee = this.getEmployeeById(id);
+    if (!employee) {
+      throw new NotFoundException('Employee not found');
+    }
+
+    const managersHierarchy: Employee[] = [];
+    this.traverseManagersHierarchy(employee, managersHierarchy);
+    return managersHierarchy;
+  }
+
+  private traverseManagersHierarchy(employee: Employee, hierarchy: Empoyee[]) {
+    if (employee.managerId !== null) {
+      const manager = this.getEmployeeById(employee.managerId);
+      if (manager) {
+        hierarchy.push(manager);
+        this.traverseManagersHierarchy(manager, hierarchy);
+      }
+    }
+  }
+
+  getDirectReportsCount(id: number): number {
+    const employee = this.getEmployeeById(id);
+    return employee ? employee.directReports.length : 0;
+  }
+
+  getIndirectReportsCount(id: number): number {
+    const employee = this.getEmployeeById(id);
+    if (!employee) {
+      throw new NotFoundException('Employee not found');
+    }
+
+    let count = 0;
+    this.traverseIndirectReports(employee, (report) => count++);
+    return count;
+  }
+
+  private traverseIndirectReports(
+    employee: Employee,
+    callback: (employee: Employee) => void,
+  ) {
+    employee.directReports.forEach((reportId) => {
+      const report = this.getEmployeeById(reportId);
+      if (report) {
+        callback(report);
+        this.traverseIndirectReports(report, callback);
+      }
+    });
+  }
+}
+
+export interface Employee {
+  id: number;
+  name: string;
+  managerId?: number | null;
+  directReports?: number[];
+}
